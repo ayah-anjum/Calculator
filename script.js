683bf5cefee0ab08dec4c33f1f8c1ffe48231d3e
ayah-anjum <ayahanjum44@gmail.com>
2025-11-19 10:20:51 +0530

    Add Calculator application files

---
+++ script.js
@@ -0,0 +1,23 @@
+let display = document.getElementById('display');
+let currentExpression = '';
+
+function appendToDisplay(value) {
+    currentExpression += value;
+    display.innerText = currentExpression;
+}
+
+function clearDisplay() {
+    currentExpression = '';
+    display.innerText = '';
+}
+
+function calculate() {
+    try {
+        let result = eval(currentExpression);
+        display.innerText = result;
+        currentExpression = result.toString();
+    } catch (error) {
+        display.innerText = 'Error';
+        currentExpression = '';
+    }
+}
